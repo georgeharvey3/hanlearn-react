4e72ce76ac3752840d3b712b29bea9be52ccefb0
georgeharvey3 <georgeharvey3@hotmail.com>
2020-10-29 09:59:36 +0000

    updated homepage look

--- src/components/Home/MainBanner/MainBanner.js
+++ src/components/Home/MainBanner/MainBanner.js
@@ -3,20 +3,20 @@ import React from 'react';
 import classes from './MainBanner.module.css';
 
 import Aux from '../../../hoc/Aux';
-import Logo from '../../Logo/Logo';
+import bannerPicture from '../../../assets/images/main-image.jpeg';
+
 
 const mainBanner = () => (
     <Aux>
         <div className={classes.MainBanner}>
-            <section>
-                <h2>A vocabulary learning application, made especially for Mandarin</h2>
-                <p>With Chinese vocabulary, there is more to learn than in other languages. Make long-lasting connections between the written forms, pronunciations and meanings of Chinese words with HanLearn</p>
-            </section>
-            <section className={classes.DesktopOnly}>
-                <div className={classes.Logo}>
-                    <Logo colour='red'/>
-                </div>
-            </section>
+            <div 
+                className={classes.Image}>
+                    <div>
+                        <h2>A vocabulary learning application, made especially for Mandarin</h2>
+                        <p>With Chinese vocabulary, there is more to learn than in other languages. Make long-lasting connections between the written forms, pronunciations and meanings of Chinese words with HanLearn</p>   
+                    </div>
+            </div>
+
         </div>
     </Aux>
 );
