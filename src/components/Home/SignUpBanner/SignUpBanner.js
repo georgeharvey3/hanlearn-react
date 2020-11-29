f8562505abf5672547578d942e174415012b8627
georgeharvey3 <georgeharvey3@hotmail.com>
2020-11-29 23:25:46 +0000

    inline signup buttons

--- src/components/Home/SignUpBanner/SignUpBanner.js
+++ src/components/Home/SignUpBanner/SignUpBanner.js
@@ -8,8 +8,10 @@ const signUpBanner = props => (
     <div className={classes.SignUpBanner}>
         <h3>Join HanLearn!</h3>
         <p>Create a free account to edit translations, add your own words and practise with spaced repitition.</p>
-        <Button colour='red' clicked={props.signUpClicked}>Sign Up</Button>
-        <Button colour='red' clicked={props.tryOutClicked}>Try it out</Button>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+            <Button colour='red' clicked={props.tryOutClicked}>Try it out</Button>
+            <Button colour='red' clicked={props.signUpClicked}>Sign Up</Button>
+        </div>
     </div>
 );
 
